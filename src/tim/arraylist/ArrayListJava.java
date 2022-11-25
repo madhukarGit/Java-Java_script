d32c9ec0e4ec0e5880d3483b83bb9edb2f861abc
madhukarGit <hmk02330@gmail.com>
2022-11-26 00:56:33 +0530

    ....

--- src/tim/arraylist/ArrayListJava.java
+++ src/tim/arraylist/ArrayListJava.java
@@ -1,5 +1,39 @@
 package tim.arraylist;
 
-public class ArrayListJava {
+import java.util.ArrayList;
 
+public class ArrayListJava {
+    private int[] myNumbers  = new int[50];
+
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public void groceryListItem(String item){
+        groceryList.add(item);
+    }
+
+    public void printGroceryList(){
+        System.out.println("You have "+groceryList.size()+" items in your list");
+        for (String s : groceryList) {
+            System.out.println((s));
+        }
+    }
+
+    private void modifyGroceryListItem(int position, String item){
+        groceryList.set(position,item);
+        System.out.println("Grocery item "+(position + 1)+" has been modified !");
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+
+    }
+
+    public String findItem(String searchItem){
+
+    }
 }
